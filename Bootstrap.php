b3fdfd232c8e65e3dc249579f46fe2d630ff1c70
xemlock <xemlock@gmail.com>
2017-03-25 22:36:27 +0100

    Reduce boilerplate code for resource initialization

--- Bootstrap.php
+++ Bootstrap.php
@@ -7,34 +7,32 @@ class ModUser_Bootstrap extends Maniple_Application_Module_Bootstrap
         return require dirname(__FILE__) . '/configs/resources.config.php';
     }
 
-    /**
-     * Register module routes
-     */
-    protected function _initRouter()
+    public function getRoutesConfig()
     {
-        /** @var Zend_Application_Bootstrap_BootstrapAbstract $bootstrap */
-        $bootstrap = $this->getApplication();
-        $bootstrap->bootstrap('FrontController');
-
-        /** @var Zend_Controller_Router_Rewrite $router */
-        $router = $bootstrap->getResource('FrontController')->getRouter();
-        $router->addConfig(new Zend_Config(require dirname(__FILE__) . '/configs/routes.config.php'));
+        return require dirname(__FILE__) . '/configs/routes.config.php';
     }
 
-    /**
-     * Register view helpers
-     */
-    protected function _initView()
+    public function getTranslationsConfig()
     {
-        /** @var Zend_Application_Bootstrap_BootstrapAbstract $bootstrap */
-        $bootstrap = $this->getApplication();
-        $bootstrap->bootstrap('View');
+        return array(
+            'adapter' => Zend_Translate::AN_ARRAY,
+            'scan'    => Zend_Translate::LOCALE_DIRECTORY,
+            'content' => __DIR__ . '/languages',
+        );
+    }
 
-        /** @var Zend_View_Abstract $view */
-        $view = $bootstrap->getResource('View');
-        $view->addScriptPath(dirname(__FILE__) . '/views/scripts');
-        $view->addHelperPath(dirname(__FILE__) . '/library/View/Helper/', 'ModUser_View_Helper_');
+    public function getViewConfig()
+    {
+        return array(
+            'scriptPaths' => dirname(__FILE__) . '/views/scripts',
+            'helperPaths' => array(
+                'ModUser_View_Helper_' => dirname(__FILE__) . '/library/View/Helper/',
+            ),
+        );
+    }
 
+    protected function _initViewRenderer()
+    {
         /** @var Zefram_Controller_Action_Helper_ViewRenderer $viewRenderer */
         $viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper('ViewRenderer');
         $viewRenderer->setViewScriptPathSpec(':module/:controller/:action.:suffix', 'mod-user');
@@ -61,25 +59,6 @@ class ModUser_Bootstrap extends Maniple_Application_Module_Bootstrap
         }
 
         // if nothing is returned, resource is not added to the container
-    }
-
-    protected function _initTranslate()
-    {
-        $bootstrap = $this->getApplication();
-        $bootstrap->bootstrap('Translate');
-
-        /** @var Zend_Translate $translate */
-        $translate = $bootstrap->getResource('Translate');
-        $locale = $translate->getLocale();
-
-        $translationsDir = dirname(__FILE__) . '/languages/' . $locale;
-
-        if (is_dir($translationsDir)) {
-            $translate->addTranslation(array(
-                'content' => $translationsDir,
-                'locale'  => $locale,
-            ));
-        }
     }
 
     protected function _initEntityManager()

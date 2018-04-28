a8661d24f258e6f2e7fe23c491ff04722fdb465f
xemlock <xemlock@gmail.com>
2018-04-28 18:13:21 +0200

    Add CurrentPassword form element

---
+++ library/ModUser/Form/Element/CurrentPassword.php
@@ -0,0 +1,36 @@
+<?php
+
+class ModUser_Form_Element_CurrentPassword extends Zend_Form_Element_Password
+{
+    /**
+     * @var ModUser_Model_UserInterface
+     */
+    protected $_user;
+
+    public function __construct(ModUser_Model_UserInterface $user, array $options = null)
+    {
+        $this->_user = $user;
+
+        if (is_object($options) && method_exists($options, 'toArray')) {
+            $options = $options->toArray();
+        }
+
+        $options = array_merge(
+            array(
+                'name'       => 'current_password',
+                'label'      => 'Current password',
+                'required'   => true,
+                'validators' => array(
+                    array(
+                        'PasswordVerify',
+                        true,
+                        array('hash' => $user->getPassword()),
+                    ),
+                ),
+            ),
+            (array) $options
+        );
+
+        parent::__construct($options);
+    }
+}

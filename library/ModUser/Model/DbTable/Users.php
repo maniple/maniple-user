83c58ab22c1a3e15d312601fa61cbb900e535e98
xemlock <xemlock@gmail.com>
2019-02-23 12:06:49 +0100

    Implement User model, annotate UserInterface

--- library/ModUser/Model/DbTable/Users.php
+++ library/ModUser/Model/DbTable/Users.php
@@ -1,8 +1,14 @@
 <?php
 
+/**
+ * @method ModUser_Model_User findRow(mixed $id)
+ * @method ModUser_Model_User createRow(array $data = array(), string $defaultSource = null)
+ */
 class ModUser_Model_DbTable_Users extends Zefram_Db_Table
 {
     const className = __CLASS__;
+
+    protected $_rowClass = ModUser_Model_User::className;
 
     protected $_name = 'users';
 }
--- library/ModUser/Model/User.php
+++ library/ModUser/Model/User.php
@@ -1,3 +1,110 @@
 <?php
 
-class_alias('Maniple\ModUser\Entity\User', 'ModUser_Model_User');
+/**
+ * @method ModUser_Model_DbTable_Users getTable()
+ */
+class ModUser_Model_User extends Zefram_Db_Table_Row implements ModUser_Model_UserInterface
+{
+    const className = __CLASS__;
+
+    protected $_tableClass = ModUser_Model_DbTable_Users::className;
+
+    public function getId()
+    {
+        return $this->user_id;
+    }
+
+    public function setId($id)
+    {
+        $this->user_id = (int) $id;
+        return $this;
+    }
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    public function setUsername($username)
+    {
+        $this->username = (string) $username;
+        return $this;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = (string) $password;
+        return $this;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = (string) $email;
+        return $this;
+    }
+
+    public function getFirstName()
+    {
+        return $this->first_name;
+    }
+
+    public function setFirstName($firstName)
+    {
+        $this->first_name = (string) $firstName;
+        return $this;
+    }
+
+    public function getLastName()
+    {
+        return $this->last_name;
+    }
+
+    public function setLastName($lastName)
+    {
+        $this->last_name = (string) $lastName;
+        return $this;
+    }
+
+    public function isActive()
+    {
+        return (bool) $this->is_active;
+    }
+
+    public function setActive($active)
+    {
+        $this->is_active = (bool) $active;
+        return $this;
+    }
+
+    public function getCreatedAt()
+    {
+        $createdAt = $this->created_at;
+
+        if (null === $createdAt) {
+            return null;
+        }
+
+        return new DateTime('@' . intval($createdAt));
+    }
+
+    public function setCreatedAt($createdAt)
+    {
+        if ($createdAt instanceof DateTime) {
+            $createdAt = $createdAt->getTimestamp();
+        }
+
+        $this->created_at = (int) $createdAt;
+        return $this;
+    }
+
+}

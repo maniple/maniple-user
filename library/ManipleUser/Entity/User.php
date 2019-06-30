d88ce4fb55d5299125e361f72f16bc96240bdd16
xemlock <xemlock@gmail.com>
2019-06-30 22:42:14 +0200

    Use underscored property names in User entity

--- library/ManipleUser/Entity/User.php
+++ library/ManipleUser/Entity/User.php
@@ -13,144 +13,144 @@ class ManipleUser_Entity_User implements ManipleUser_Entity_UserInterface
      * @Column(name="user_id", type="integer")
      * @GeneratedValue(strategy="AUTO")
      */
-    protected $id;
+    protected $_id;
 
     /**
      * @Column(name="is_active", type="boolint", nullable=false)
      */
-    protected $active = true;
+    protected $_active = true;
 
     /**
      * @Column(name="created_at", type="epoch")
      */
-    protected $createdAt;
+    protected $_createdAt;
 
     /**
      * @Column(name="username", type="string", length=255, unique=true)
      */
-    protected $username;
+    protected $_username;
 
     /**
      * @Column(name="email", type="string", length=255, unique=true)
      */
-    protected $email;
+    protected $_email;
 
     /**
      * @Column(name="password", type="string", length=255)
      */
-    protected $password;
+    protected $_password;
 
     /**
      * @Column(name="first_name", type="string", length=255, nullable=true)
      */
-    protected $firstName;
+    protected $_firstName;
 
     /**
      * @Column(name="last_name", type="string", length=255, nullable=true)
      */
-    protected $lastName;
+    protected $_lastName;
 
     /**
      * @Column(name="middle_name", type="string", length=255, nullable=true)
      */
-    protected $middleName;
+    protected $_middleName;
 
     public function getId()
     {
-        return $this->id;
+        return $this->_id;
     }
 
-    public function setId($id)
+    public function setId($_id)
     {
-        $this->id = $id;
+        $this->_id = $_id;
         return $this;
     }
 
     public function getUsername()
     {
-        return $this->username;
+        return $this->_username;
     }
 
     public function setUsername($username)
     {
-        $this->username = $username;
+        $this->_username = $username;
         return $this;
     }
 
     public function getPassword()
     {
-        return $this->password;
+        return $this->_password;
     }
 
     public function setPassword($password)
     {
-        $this->password = $password;
+        $this->_password = $password;
         return $this;
     }
 
     public function getEmail()
     {
-        return $this->email;
+        return $this->_email;
     }
 
     public function setEmail($email)
     {
-        $this->email = $email;
+        $this->_email = $email;
         return $this;
     }
 
     public function isActive()
     {
-        return $this->active;
+        return $this->_active;
     }
 
     public function setActive($active)
     {
-        $this->active = (bool) $active;
+        $this->_active = (bool) $active;
         return $this;
     }
 
     public function getCreatedAt()
     {
-        return $this->createdAt;
+        return $this->_createdAt;
     }
 
     public function setCreatedAt($createdAt)
     {
-        $this->createdAt = $createdAt;
+        $this->_createdAt = $createdAt;
         return $this;
     }
 
     public function getFirstName()
     {
-        return $this->firstName;
+        return $this->_firstName;
     }
 
     public function setFirstName($firstName)
     {
-        $this->firstName = $firstName;
+        $this->_firstName = $firstName;
         return $this;
     }
 
     public function getLastName()
     {
-        return $this->lastName;
+        return $this->_lastName;
     }
 
     public function setLastName($lastName)
     {
-        $this->lastName = $lastName;
+        $this->_lastName = $lastName;
         return $this;
     }
 
     public function setMiddleName($middleName)
     {
-        $this->middleName = $middleName;
+        $this->_middleName = $middleName;
         return $this;
     }
 
     public function getMiddleName()
     {
-        return $this->middleName;
+        return $this->_middleName;
     }
 }
